ab36b24556866fc8bafdc600c2f7e0a4f2d63d2a
Nico Larena <71468011+NicLarUniversidad@users.noreply.github.com>
2022-11-12 18:40:14 -0300

    .

--- Prueba/src/main/java/ar/com/file/system/servidorarchivo/demo/controller/TestController.java
+++ Prueba/src/main/java/ar/com/file/system/servidorarchivo/demo/controller/TestController.java
@@ -4,10 +4,7 @@ import ar.com.file.system.servidorarchivo.demo.entities.TestEntity;
 import ar.com.file.system.servidorarchivo.demo.models.TestModel;
 import ar.com.file.system.servidorarchivo.demo.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
--- Prueba/src/main/java/ar/com/file/system/servidorarchivo/demo/entities/TestEntity.java
+++ Prueba/src/main/java/ar/com/file/system/servidorarchivo/demo/entities/TestEntity.java
@@ -2,10 +2,15 @@ package ar.com.file.system.servidorarchivo.demo.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class TestEntity {
     @Id
     private int id;
     private String name;
+    private String path;
+    private int usuario;
+
 }
