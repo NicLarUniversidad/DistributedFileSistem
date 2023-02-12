031bdae8731cfb73e3c6d74d5e85eb05bc5ce32b
dbm349 <delmore97@hotmail.com>
2023-02-12 10:38:13 -0300

    agrego metodo cache

---
+++ FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/controller/ServerFileController.java
@@ -0,0 +1,54 @@
+package ar.com.file.system.servidorarchivo.demo.controller;
+
+import ar.com.file.system.servidorarchivo.demo.cruds.File;
+import ar.com.file.system.servidorarchivo.demo.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class ServerFileController {
+    @Autowired
+    private FileRepository fileRepository;
+    private File file;
+
+    @PostMapping("/ArchivoCreate")
+    public File CreateFile (@RequestBody String nombreArchivo){
+        var newFile = new File();
+        newFile.setNombreArchivo(nombreArchivo);
+        newFile.setNombreRutaDirectorio(" ");
+        newFile.setTamaño("10kb");
+        newFile.setFormato("txt");
+        newFile.setTipo("1");
+        newFile.setActivo(false);
+        return fileRepository.save(newFile);
+    }
+
+
+    @PostMapping("/ArchivoDelete")
+    public String DeleteFile (@RequestBody File archivo){
+        archivo.setActivo(false);
+        return "Archivo eliminado";
+    }
+
+
+    @GetMapping("/Archivos")
+    public Iterable <File> listFiles (){
+        return fileRepository.findAll();
+    }
+
+
+/*
+    @Cacheable (cacheNames = "archivos")
+    public File findById(int id){
+        File f = this.fileRepository.stream().filter(it -> it.getId()==id).findFirst().orElseThrow(RuntimeException::new);
+        logger.info("Item {} fue recuperado de la base", f.toString());
+        return f;
+    }
+*/
+
+
+
+
+
+}
---
+++ FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/controller/ServerUserController.java
@@ -0,0 +1,53 @@
+package ar.com.file.system.servidorarchivo.demo.controller;
+
+import ar.com.file.system.servidorarchivo.demo.cruds.User;
+import ar.com.file.system.servidorarchivo.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerUserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    private User user;
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @PostMapping ("/UserCreate")
+
+    public User registerUser (@RequestBody String nombre){
+        var newUser = new User();
+        newUser.setNombre(nombre);
+        newUser.setApellido("Morello");
+        newUser.setEmail("delfi@hotmail.com");
+        newUser.setDireccion("134");
+        newUser.setActivo(false);
+        return userRepository.save(newUser);
+    }
+
+
+    @PostMapping("/UserDelete")
+    public String DeleteFile (@RequestBody User usuario){
+        usuario.setActivo(false);
+        return "Usuario eliminado";
+    }
+
+    @GetMapping ("/User")
+    public Iterable <User> listUser (){
+        return userRepository.findAll();
+    }
+
+/*
+    @Cacheable (cacheNames = "personas")
+    public User findById(int id){
+        User u = this.userRepository.stream().filter(it -> it.getId()==id).findFirst().orElseThrow(RuntimeException::new);
+        logger.info("Item {} fue recuperado de la base", u.toString());
+        return u;
+    }
+*/
+
+
+}
