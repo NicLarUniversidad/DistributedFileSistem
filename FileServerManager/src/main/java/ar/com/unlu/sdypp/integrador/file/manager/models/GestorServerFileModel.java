316510e7403473171fa5a7a05af073825b191428
dbm349 <delmore97@hotmail.com>
2024-01-02 17:31:41 -0300

    agrego tablas de las entidades

---
+++ FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/models/GestorServerFileModel.java
@@ -0,0 +1,19 @@
+package ar.com.unlu.sdypp.integrador.file.manager.models;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@Entity
+public class GestorServerFileModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private boolean status;
+    private boolean saturado;
+}
