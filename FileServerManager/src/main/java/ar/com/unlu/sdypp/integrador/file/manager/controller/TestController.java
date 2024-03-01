5dc9a823c09cd34c4b03c0fa811631a6a0473c92
NicLarUniversidad <71468011+NicLarUniversidad@users.noreply.github.com>
2024-03-01 18:45:17 -0300

    Corrección modelos

--- FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/controller/TestController.java
+++ FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/controller/TestController.java
@@ -1,6 +1,5 @@
 package ar.com.unlu.sdypp.integrador.file.manager.controller;
 
-import ar.com.file.system.servidorarchivo.demo.models.TestModel;
 import ar.com.unlu.sdypp.integrador.file.manager.entities.TestEntity;
 import ar.com.unlu.sdypp.integrador.file.manager.services.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
--- FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/repositories/ClientRepository.java
+++ FileServerManager/src/main/java/ar/com/unlu/sdypp/integrador/file/manager/repositories/ClientRepository.java
@@ -1,19 +1,26 @@
 package ar.com.unlu.sdypp.integrador.file.manager.repositories;
 
-import ar.com.unlu.sdypp.integrador.file.manager.servers.LoadBalancerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import ar.com.unlu.sdypp.integrador.file.manager.cruds.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Repository
 public class ClientRepository {
 
     @Query(value = "SELECT u.nombre, u.apellido FROM User u")
-    List<User> findByFirstnameAndLastname(String firstname, String lastname);
+    List<User> findByFirstnameAndLastname(String firstname, String lastname) {
+        return null;
+    }
 
     @Query(value = "SELECT u.permiso FROM User u")
-    List<User> findBypermiso(String permiso);
+    List<User> findBypermiso(String permiso) {
+        return null;
+    }
 
     @Query(value = "SELECT u.nombre, u.apellido FROM User u WHERE u.permiso=2")
-    List<int> getUsersWherePermiso2();
+    List<int> getUsersWherePermiso2() {
+        return null;
+    }
 }
