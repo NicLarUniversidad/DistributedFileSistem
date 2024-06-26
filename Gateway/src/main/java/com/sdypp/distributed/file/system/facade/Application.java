263e88bed9bc98dcf8130af9b1ced9644a545665
Nico Larena <nicolas.larena.work@gmail.com>
2024-06-26 04:21:36 -0300

    Creados los Manifest para poder compilar los .jar

---
+++ Gateway/src/main/java/com/sdypp/distributed/file/system/facade/Application.java
@@ -0,0 +1,15 @@
+package com.sdypp.distributed.file.system.facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Declaración genérica de aplicación Spring-boot, inicia la aplicación con el framework...
+//@SpringBootApplication
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
