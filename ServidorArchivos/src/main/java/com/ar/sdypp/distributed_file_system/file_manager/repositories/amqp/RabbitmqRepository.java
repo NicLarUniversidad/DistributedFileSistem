c1c4da3da442b792f2cff749d5405e3a4733cf78
Nico Larena <nicolas.larena.work@gmail.com>
2024-07-01 10:01:37 -0300

    Corregida asignación de variables de entorno en constructor

--- ServidorArchivos/src/main/java/com/ar/sdypp/distributed_file_system/file_manager/repositories/amqp/RabbitmqRepository.java
+++ ServidorArchivos/src/main/java/com/ar/sdypp/distributed_file_system/file_manager/repositories/amqp/RabbitmqRepository.java
@@ -1,20 +1,27 @@
 package com.ar.sdypp.distributed_file_system.file_manager.repositories.amqp;
 
 import com.ar.sdypp.distributed_file_system.file_manager.models.FileModel;
+import com.ar.sdypp.distributed_file_system.file_manager.services.FileService;
 import com.google.gson.Gson;
 import com.rabbitmq.client.*;
 import org.apache.logging.log4j.message.Message;
 import org.jasypt.util.text.StrongTextEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
 
@@ -25,31 +32,46 @@ public class RabbitmqRepository {
 
     public final static Logger logger = LoggerFactory.getLogger(RabbitmqRepository.class);
 
-    private final Connection connection;
-    private final Channel channel;
+    private Connection connection;
+    private Channel channel;
     //Se asigna el valor por variable del programa, en el application.properties
-    @Value("${sdypp.rabbitmq.host:localhost}")
-    private String host = "localhost";
-    @Value("${sdypp.rabbitmq.queue.name:GuardadoArchivo}")
-    private String queueName = "GuardadoArchivo";
+    @Value("${sdypp.rabbitmq.host}")
+    private String host;
+    @Value("${sdypp.rabbitmq.queue.name}")
+    private String queueName;
     //Cantidad máxima de mensajes que los consumidores pueden consumir a la vez
-    @Value("${sdypp.rabbitmq.consumer.prefetch-count:1}")
+    @Value("${sdypp.rabbitmq.consumer.prefetch-count}")
     private int prefetchCount = 1;
     @Value("${sdypp.rabbitmq.queue.exchange-name:default-exchange}")
-    private String exchangeName = "default-exchange";
-    @Value("${sdypp.rabbitmq.queue.topics.finish-work:works.finished}")
-    private String finishedWorkTopic = "works.finished";
-    @Value("${sdypp.rabbitmq.queue.username:guest}")
-    private String username = "guest";
-    @Value("${sdypp.rabbitmq.queue.password:123}")
-    private String password = "123";
-    private final StrongTextEncryptor textEncryptor;
+    private String exchangeName;
+    @Value("${sdypp.rabbitmq.queue.topics:finish-work}")
+    private String finishedWorkTopic;
+    @Value("${sdypp.rabbitmq.queue.username}")
+    private String username;
+    @Value("${sdypp.rabbitmq.queue.password}")
+    private String password;
+    private StrongTextEncryptor textEncryptor;
 
-    public RabbitmqRepository() throws IOException, TimeoutException {
+    @Autowired
+    public RabbitmqRepository(Environment env) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(host);
-        factory.setUsername(username);
-        factory.setPassword(password);
+        this.host = env.getProperty("sdypp.rabbitmq.host");
+        this.queueName = env.getProperty("sdypp.rabbitmq.queue.name");
+        this.exchangeName = env.getProperty("sdypp.rabbitmq.queue.exchange-name");
+        this.queueName = env.getProperty("sdypp.rabbitmq.queue.name");
+        this.username = env.getProperty("sdypp.rabbitmq.queue.username");
+        this.password = env.getProperty("sdypp.rabbitmq.queue.password");
+        this.finishedWorkTopic = env.getProperty("sdypp.rabbitmq.queue.topics");
+        this.prefetchCount = Integer.parseInt(Objects.requireNonNull(env.getProperty("sdypp.rabbitmq.consumer.prefetch-count")));
+        String uri = env.getProperty("sdypp.rabbitmq.uri");
+        if (uri != null) {
+            factory.setUri(uri);
+        }
+        else {
+            factory.setHost(host);
+            factory.setUsername(username);
+            factory.setPassword(password);
+        };
         connection = factory.newConnection();
         channel = connection.createChannel();
         channel.queueDeclare(queueName, true, false, false, null);
@@ -78,33 +100,43 @@ public class RabbitmqRepository {
     }
 
     public void listenQueue(String queue) throws IOException {
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(queue, true, false, false, null);
+        try {
 
-        channel.queueBind(queueName, exchangeName, finishedWorkTopic);
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            logger.info("Message received: " + message);
-            //Acá se debe procesar los mensajes recibidos
-            Gson gson = new Gson();
-            FileModel file = gson.fromJson(message, FileModel.class);
-            String path = file.getUsername().replace(":", "/").replace(".", "/");
-            File filePath = new File(path);
-            //filePath.exists();
-            Files.createDirectories(Paths.get(username.replace(":", "/").replace(".", "/")));
-            File newFIle = new File(filePath, file.getName());
-            logger.info(newFIle.getAbsolutePath());
-            if (!newFIle.exists()) {
-                newFIle.createNewFile();
-            }
-            var encryptData = textEncryptor.encrypt(file.getContent()).getBytes();
-            Files.write(newFIle.toPath(), encryptData);
-            //Se avisa que se procesó el mensaje
-            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-            //TODO: ACK que se guardó el archivo
-            //this.send(); objeto nuevo que comunique donde se guardó la parte, con un id y path
-            //Debe estar en un tópico separado
-        };
-        channel.basicConsume(queue, false, deliverCallback, consumerTag -> { });
+            logger.info("Contectando con RabbitMQ... Host: " + this.host);
+            Channel channel = connection.createChannel();
+            logger.info("Declarando cola...");
+            channel.queueDeclare(queue, true, false, false, null);
+
+            logger.info("Bind...");
+            channel.queueBind(queueName, exchangeName, finishedWorkTopic);
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                logger.info("Message received: " + message);
+                //Acá se debe procesar los mensajes recibidos
+                Gson gson = new Gson();
+                FileModel file = gson.fromJson(message, FileModel.class);
+                String path = file.getUsername().replace(":", "/").replace(".", "/");
+                File filePath = new File(path);
+                //filePath.exists();
+                Files.createDirectories(Paths.get(username.replace(":", "/").replace(".", "/")));
+                File newFIle = new File(filePath, file.getName());
+                logger.info(newFIle.getAbsolutePath());
+                if (!newFIle.exists()) {
+                    newFIle.createNewFile();
+                }
+                var encryptData = textEncryptor.encrypt(file.getContent()).getBytes();
+                Files.write(newFIle.toPath(), encryptData);
+                //Se avisa que se procesó el mensaje
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                //TODO: ACK que se guardó el archivo
+                //this.send(); objeto nuevo que comunique donde se guardó la parte, con un id y path
+                //Debe estar en un tópico separado
+            };
+            logger.info("Basic consume...");
+            channel.basicConsume(queue, false, deliverCallback, consumerTag -> { });
+        }
+        catch (IOException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
