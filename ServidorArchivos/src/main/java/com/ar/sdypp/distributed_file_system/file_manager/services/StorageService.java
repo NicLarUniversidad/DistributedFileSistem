e920294ce4e6690a20a811b21a122f02b4c3a14b
Nico Larena <nicolas.larena.work@gmail.com>
2024-07-02 12:46:27 -0300

    Get file from GPC Storage

--- ServidorArchivos/src/main/java/com/ar/sdypp/distributed_file_system/file_manager/services/StorageService.java
+++ ServidorArchivos/src/main/java/com/ar/sdypp/distributed_file_system/file_manager/services/StorageService.java
@@ -2,18 +2,26 @@ package com.ar.sdypp.distributed_file_system.file_manager.services;
 
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.ReadChannel;
 import com.google.cloud.storage.*;
 import com.google.common.io.CharSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileUrlResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StreamUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.HashMap;
@@ -24,22 +32,11 @@ public class StorageService {
 
     private static Logger logger = LoggerFactory.getLogger(StorageService.class);
 
-    public static String cred = "";
+    @Value("${sdypp.storage.credentials:}")
+    public String cred = "";
 
     public String saveFile(byte[] fileContent, String fileName) throws IOException {
-//        Storage storage = StorageOptions.getDefaultInstance().getService();
-//        Bucket bucket = storage.create(BucketInfo.of("gs://sdypp-file-system"));
-//        Blob blob = bucket.create(fileName, fileContent);
-//        return blob.getMediaLink();
-
-        //ClassPathResource classPathResource = new ClassPathResource("classpath:storage-credentials.json");
-        StorageOptions storageOptions = StorageOptions.newBuilder()
-                .setProjectId("hybrid-hawk-428007-f9")
-                .setCredentials(GoogleCredentials.fromStream(
-                        //classPathResource.getInputStream()
-                        new ByteArrayInputStream(cred.getBytes(StandardCharsets.UTF_8))
-                )).build();
-        Storage storage = storageOptions.getService();
+        Storage storage = this.getStorage();
         long fileSize = fileContent.length;
 
         Map<String, String> metadata = new HashMap<>();
@@ -63,5 +60,26 @@ public class StorageService {
         }
 
 
+    }
+
+    public String getFile(String fileName) throws IOException {
+//        FileUrlResource gcsFile = new FileUrlResource("https://storage.cloud.google.com/sdypp-file-system/" + fileName);
+//        return StreamUtils.copyToString(
+//                gcsFile.getInputStream(),
+//                Charset.defaultCharset());
+        Storage storage = this.getStorage();
+        Blob blob = storage.get("sdypp-file-system", fileName);
+        return new String(blob.getContent());
+    }
+
+
+    private Storage getStorage() throws IOException {
+        StorageOptions storageOptions = StorageOptions.newBuilder()
+                .setProjectId("hybrid-hawk-428007-f9")
+                .setCredentials(GoogleCredentials.fromStream(
+                        //classPathResource.getInputStream()
+                        new ByteArrayInputStream(cred.getBytes(StandardCharsets.UTF_8))
+                )).build();
+        return storageOptions.getService();
     }
 }
