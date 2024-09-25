49d29148112100840cddba8fa21125aa301aa5fd
NicLarUniversidad <nic.lar.universidad@gmail.com>
2024-09-25 12:51:48 -0300

    Update FilePart.java

--- Gateway/src/main/java/com/sdypp/distributed/file/system/facade/models/FilePart.java
+++ Gateway/src/main/java/com/sdypp/distributed/file/system/facade/models/FilePart.java
@@ -3,8 +3,10 @@ package com.sdypp.distributed.file.system.facade.models;
 import lombok.Data;
 import org.springframework.core.io.Resource;
 
+import java.io.Serializable;
+
 @Data
-public class FilePart {
+public class FilePart implements Serializable {
     private byte[] resource;
     private Integer number;
     private Boolean hasNext;
